2c0ee9b83d72ac37514cbeacce00a5b664d6a1c9
Vyoman Jain <75789232+VyoJ@users.noreply.github.com>
2023-07-25 12:56:23 +0530

    Updated Footer and About

--- v2/v2/src/Routes/About.js
+++ v2/v2/src/Routes/About.js
@@ -5,7 +5,7 @@ function About() {
     return (
         <div>
             <Navbar/>
-            <div class="bg-black p-12">
+            <div class="bg-[url('https://cdn.discordapp.com/attachments/1025099364584149094/1133297922985959455/About2.jpg')] p-12">
                 <h1 class="text-4xl font-bold text-blue-500 mb-4">About Us</h1>
                 <p class="text-xl text-white ml-6"> Welcome to the Quantum Computing Research Group, a dynamic and innovative team at the forefront of advancing quantum computing technologies. Our group specializes in the fascinating field of quantum computing with a focus on experimental physics. We are dedicated to pushing the boundaries of what is possible in this exciting and rapidly evolving area of research.</p>
                 <p class="text-xl text-white mt-4 ml-6">In this introduction, we will provide an overview of our research group, describing our mission, the expertise of our team members, and the cutting-edge projects we are currently engaged in. We will also highlight the significance of our work in advancing quantum computing and its potential impact on various fields, including cryptography, optimization problems, and machine learning.</p>
